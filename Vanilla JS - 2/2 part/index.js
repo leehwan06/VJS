fc73ec98dcc2774ae4ce3e321b2dcd2a6f8c8608
leehwan06 <hooniggee75@gmail.com>
2020-09-25 23:20:35 +0900

    200925 FS

---
+++ Vanilla JS - 2/2 part/index.js
@@ -0,0 +1,17 @@
+const title = document.getElementById("title");
+
+console.log(title); 
+
+//javascript는 우리의 html을 모두 객체화시킨다
+/*
+console.log의
+저 console 처럼 쓸수있다
+*/
+
+//그럼 뒤의 log 처럼 쓸수있는거는 뭐가잇을까?
+//간단한 innerHTML이 있다
+
+title.innerHTML = "Hi  From JS";
+//자 이코드가 무었을 뜻하냐 
+//원래있던 걸 바꾸는거야
+//Hi From Js로
